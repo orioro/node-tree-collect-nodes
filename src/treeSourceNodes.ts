ef1ed0c296a8c77d32c7bd2f43bf4f5b9005c2d0
Simon Fan <simon@orioro.design>
2021-01-22 13:02:04 -0300

    Adds typings

--- src/treeSourceNodes.ts
+++ src/treeSourceNodes.ts
@@ -1,9 +1,19 @@
 import { isPlainObject } from 'lodash'
 import { cascadeExec, test } from '@orioro/cascade'
 
+export type NodeResolverContext = {
+  path: string,
+  [key: string]: any
+}
+
+export type Node = {
+  path: string,
+  [key: string]: any
+}
+
 export const arrayNodeResolver = (deepFirst = false) => ([
   value => Array.isArray(value),
-  (value:any[], context) => (
+  (value:any[], context:NodeResolverContext):Node[] => (
     value.reduce((acc, item, index) => {
       return [
         ...acc,
@@ -21,7 +31,7 @@ export const arrayNodeResolver = (deepFirst = false) => ([
 
 export const objectNodeResolver = (deepFirst = false) => ([
   value => isPlainObject(value),
-  (value, context) => (
+  (value:{ [key: string]: any }, context:NodeResolverContext):Node[] => (
     Object.keys(value).reduce((acc, key) => {
       return [
         ...acc,
@@ -40,7 +50,7 @@ export const objectNodeResolver = (deepFirst = false) => ([
 export const defaultNodeResolver = (
   getNodeContextData = context => ({ path: context.path })
 ) => ([
-  (value, context) => ([{
+  (value:any, context:NodeResolverContext):Node[] => ([{
     ...getNodeContextData(context),
     value
   }])
@@ -49,4 +59,4 @@ export const defaultNodeResolver = (
 export const treeSourceNodes = (
   value,
   context
-) => cascadeExec(test, context.resolvers, value, context)
+):Node[] => cascadeExec(test, context.resolvers, value, context)
